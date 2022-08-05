58be4a0c973a420c47cec76f07dc8ef31a4836de
Derrak Richard <derrak@gmail.com>
2022-08-05 09:03:54 -0700

    update machine name reference to machineName

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -23,7 +23,7 @@ namespace Factory.Controllers
 
     public ActionResult Create()
     {
-      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Name");
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "MachineName");
       return View();
     }
 
